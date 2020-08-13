ef32490894d31f0b1021ea36ee2df229765747ab
nahlaahmed97 <nahla.elaskry97@gmail.com>
2020-08-13 23:23:53 +0200

    Updates

---
+++ precache-manifest.c2f7f466e6905fc7ea72e6484bc275de.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "18a8cc49775b8f3e05432495e848f63f",
+    "url": "/Movie-React/index.html"
+  },
+  {
+    "revision": "081281f6c8f599d55624",
+    "url": "/Movie-React/static/css/main.27be6563.chunk.css"
+  },
+  {
+    "revision": "fe4dec88b4311d4cc60d",
+    "url": "/Movie-React/static/js/2.fd08b29f.chunk.js"
+  },
+  {
+    "revision": "c6d01cb19213691bc81a274434d00fe7",
+    "url": "/Movie-React/static/js/2.fd08b29f.chunk.js.LICENSE"
+  },
+  {
+    "revision": "081281f6c8f599d55624",
+    "url": "/Movie-React/static/js/main.eeb5245f.chunk.js"
+  },
+  {
+    "revision": "a4827d239e167b7ba963",
+    "url": "/Movie-React/static/js/runtime-main.15b3730a.js"
+  }
+]);
